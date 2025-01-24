0a2e8110c1dc71fc5632b4dd886b599f1d75bcee
Jéssica Oliveira <jessicamelestudante@gmail.com>
2025-01-24 17:26:56 -0300

    Primeiro commit

---
+++ build/Desktop-Debug/CMakeFiles/Escultor3d-Parte2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Escultor3d-Parte2.dir/main.cpp.o"
+  "CMakeFiles/Escultor3d-Parte2.dir/main.cpp.o.d"
+  "Escultor3d-Parte2"
+  "Escultor3d-Parte2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Escultor3d-Parte2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/Desktop-Debug/CMakeFiles/Escultor3d-Parte2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Escultor3d-Parte2.
+# This may be replaced when dependencies are built.
