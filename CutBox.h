991fa732082e2bf97f2073549f7c2e3b39f3c8a9
Jéssica Oliveira <jessicamelestudante@gmail.com>
2025-01-27 16:35:47 -0300

    Organização

--- CutBox.h
+++ CutBox.h
@@ -3,11 +3,7 @@
 
 #include "FiguraGeometrica.h"
 
-<<<<<<< HEAD
-class CutBox : public FiguraGeometrica{
-=======
 class CutBox : public FiguraGeometrica {
->>>>>>> 45e38a930d35edb6585ed10fbfa69fb911b41849
     int x0, x1, y0, y1, z0, z1;
 public:
     CutBox(int x0, int x1, int y0, int y1, int z0, int z1);
