ec00f62310079326840e6a82916b7be30e96c7a5
Alvaro <alvinhofd@gmail.com>
2025-01-27 15:18:38 -0300

    Update PutSphere.h

--- PutSphere.h
+++ PutSphere.h
@@ -1,1 +1,15 @@
+#ifndef PUTSPHERE_H
+#define PUTSPHERE_H
 
+#include "figurageometrica.h"
+
+class PutSphere : public FiguraGeometrica
+{
+    int xcenter, ycenter, zcenter, radius;
+public:
+    PutSphere(int xcenter, int ycenter, int zcenter, int radius, float r, float g, float b, float a);
+    void draw(Scultor &t);
+    ~PutSphere(){};
+};
+
+#endif // PUTSPHERE_H
