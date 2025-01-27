49e73e763e1e0b8aede1a9333c76d02a502babd9
Alvaro <alvinhofd@gmail.com>
2025-01-27 15:20:19 -0300

    Update CutBox.cpp
    ops.. rsrs

--- CutBox.cpp
+++ CutBox.cpp
@@ -1,11 +1,22 @@
-#include "cutVoxel.h"
+#include "cutbox.h"
 
-CutVoxel::Cutvoxel(int x, int y, int z){
-    this->x=x; 
-    this->y=y; 
-    this->z=z;
+CutBox::CutBox(int x0, int x1, int y0, int y1, int z0, int z1)
+{
+    this->x0=x0; 
+    this->x1=x1; 
+    this->y0=y0; 
+    this->y1=y1; 
+    this->z0=z0; 
+    this->z1=z1;
 }
 
-void CutVoxel::draw(Sculptor &t){
-    t.cutVoxel(x, y, z);
+void CutBox::draw(Scultor &t) // Como no "cut" a intenção é passar a borracha no desenho, não há necessidade de usar rgba
+{
+    for(int x = x0; x<x1; x++){
+       for (int y = y0; y<y1; y++){
+           for (int z = z0; z<z1; z++){
+               t.cutVoxel(x,y,z);
+           }
+       }
+    }
 }
