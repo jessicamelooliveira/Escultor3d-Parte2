51a2fde092a6fe42cc0d63c7bacd6e520c929cb6
Alvaro <alvinhofd@gmail.com>
2025-01-27 15:19:43 -0300

    Update CutBox.cpp

--- CutBox.cpp
+++ CutBox.cpp
@@ -1,1 +1,11 @@
+#include "cutVoxel.h"
 
+CutVoxel::Cutvoxel(int x, int y, int z){
+    this->x=x; 
+    this->y=y; 
+    this->z=z;
+}
+
+void CutVoxel::draw(Sculptor &t){
+    t.cutVoxel(x, y, z);
+}
