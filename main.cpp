acebe67a4ef0c5bdd4ae3220ae52c22699eba56b
Jéssica Oliveira <jessicamelestudante@gmail.com>
2025-01-27 17:40:22 -0300

    mudanças

--- main.cpp
+++ main.cpp
@@ -18,10 +18,38 @@ int main(){
 
     std::vector <FiguraGeometrica*> figuras;
 
+    // Verificar se o arquivo esta aberto corretamente
     std::ifstream fin;
     fin.open("figura.txt");
     if(!fin.is_open()){
         exit(0);
+    }
+
+    while(fin.good()){
+        fin >> s;
+        if(fin.good()){
+            //     std::cout << "leu: " << s << std::endl;
+            if(s.compare("retangulo") == 0){
+                float x1, y1, largura, altura;
+                fin >> x1 >> y1 >> largura >> altura;
+                figuras.push_back(
+                    new Retangulo(x1,y1,largura,altura));
+            }
+            else if(s.compare("reta") == 0){
+                float x1, y1, x2, y2;
+                fin >> x1 >> y1 >> x2 >> y2;
+                figuras.push_back(
+                    new Reta(x1,y1,x2,y2));
+
+            }
+            else if(s.compare("circulo") == 0){
+                float xc, yc, raio;
+                fin >> xc >> yc >> raio;
+                figuras.push_back(
+                    new Circulo(xc,yc,raio));
+
+            }
+        }
     }
 
     std::string s;
