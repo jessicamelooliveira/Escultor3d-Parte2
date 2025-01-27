f9ad75c24642235cc4b4de6aa0f794a495f735c5
Alvaro <alvinhofd@gmail.com>
2025-01-27 16:29:21 -0300

    Update CutBox.h

--- CutBox.h
+++ CutBox.h
@@ -3,8 +3,7 @@
 
 #include "figurageometrica.h"
 
-class CutBox : public FiguraGeometrica
-{
+class CutBox : public FiguraGeometrica {
     int x0, x1, y0, y1, z0, z1;
 public:
     CutBox(int x0, int x1, int y0, int y1, int z0, int z1);
